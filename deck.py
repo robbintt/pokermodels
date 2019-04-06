5dfd0b6963ca46f5eff82d089b5ac33299d87702
Trent Robbins <robbintt@gmail.com>
2019-04-05 23:36:04 -0700

    Finished up unit tests.

--- deck.py
+++ deck.py
@@ -1,6 +1,7 @@
 '''
 '''
 import unittest
+import random
 
 
 SUITS = ('S', 'C', 'H', 'D')
@@ -49,9 +50,11 @@ class Deck(object):
         self.ranks = ranks
         self.shuffled = shuffled
         self.deck = [Card(s, r) for s in self.suits for r in self.ranks]
+        if self.shuffled:
+            random.shuffle(self.deck)
 
     
-class DeckPropertiesTest(unittest.TestCase):
+class StandardDeckPropertiesTest(unittest.TestCase):
 
     def setUp(self):
         self.shuffled_deck = Deck(SUITS, RANKS, shuffled=True)
@@ -59,6 +62,12 @@ class DeckPropertiesTest(unittest.TestCase):
 
     def test_count_deck(self):
         self.assertEqual(len(self.shuffled_deck.deck), 52)
+
+    def test_count_kings(self):
+        self.assertEqual(len([card for card in self.shuffled_deck.deck if card.rank == 'K']), 4)
+
+    def test_count_spades(self):
+        self.assertEqual(len([card for card in self.shuffled_deck.deck if card.suit == 'S']), 13)
 
 
 class TexasHoldemHand(object):
@@ -78,6 +87,8 @@ class TexasHoldemHand(object):
 if __name__ == '__main__':
     '''
     '''
+    print("Unshuffled", ["{}{}".format(card.suit, card.rank) for card in Deck(SUITS, RANKS, shuffled=False).deck])
+    print("Shuffled", ["{}{}".format(card.suit, card.rank) for card in Deck(SUITS, RANKS, shuffled=True).deck])
     unittest.main()
     
 
