0b80427fe42dbc2ceabdde50e2d348abd7c2523e
Trent Robbins <robbintt@gmail.com>
2019-04-06 15:26:24 -0700

    Removed some early smoke test cruft.

--- poker.py
+++ poker.py
@@ -17,30 +17,6 @@ if __name__ == '__main__':
     '''
     from tests.poker_tests import HandEvaluatorTest, StandardDeckPropertiesTest
 
-    shuffled_deck = Deck(SUITS, RANKS)
-    unshuffled_deck = Deck(SUITS, RANKS, shuffled=False)
-
-    hand = Hand(
-            shuffled_deck.deal(), 
-            shuffled_deck.deal())
-    community_cards = CommunityCards(
-            shuffled_deck.deal(), 
-            shuffled_deck.deal(), 
-            shuffled_deck.deal(), 
-            shuffled_deck.deal(), 
-            shuffled_deck.deal())
-    hand_evaluator_object = HandEvaluator(hand, community_cards)
-
-    print("Hand: {}".format(hand))
-    print("Community Cards: {}".format(community_cards))
-    print("Hand Evaluator Cards: {}".format(hand_evaluator_object))
-
-    hand_evaluator_object.order_cards()
-    print("Hand Evaluator Cards: {}".format(hand_evaluator_object))
-    print("The hand has a straight? {}".format(hand_evaluator_object.eval_straight()))
-
-    hand_evaluator_object.eval_straight()
-
     unittest.main(verbosity=2)
     
 
