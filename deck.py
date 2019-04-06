23ec3e6656191b93c8aaa5303e3fec9ad25fcab6
Trent Robbins <robbintt@gmail.com>
2019-04-05 23:28:34 -0700

    Added deck class.

---
+++ deck.py
@@ -0,0 +1,83 @@
+'''
+'''
+import unittest
+
+
+SUITS = ('S', 'C', 'H', 'D')
+RANKS = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+
+SUIT_NAMES = {
+    'S': 'spades',
+    'C': 'clubs',
+    'H': 'hearts',
+    'D': 'diamonds'
+}
+
+RANK_NAMES = {
+    '2' : 'two',
+    '3' : 'three',
+    '4' : 'four',
+    '5' : 'five',
+    '6' : 'six',
+    '7' : 'seven',
+    '8' : 'eight',
+    '9' : 'nine',
+    '10' : 'ten',
+    'J' : 'jack',
+    'Q' : 'queen',
+    'K' : 'king',
+    'A' : 'ace'
+}
+
+
+class Card(object):
+    '''
+    '''
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    
+
+class Deck(object):
+    '''
+    '''
+
+    def __init__(self, suits, ranks, shuffled=True):
+        '''
+        '''
+        self.suits = suits
+        self.ranks = ranks
+        self.shuffled = shuffled
+        self.deck = [Card(s, r) for s in self.suits for r in self.ranks]
+
+    
+class DeckPropertiesTest(unittest.TestCase):
+
+    def setUp(self):
+        self.shuffled_deck = Deck(SUITS, RANKS, shuffled=True)
+        self.unshuffled_deck = Deck(SUITS, RANKS, shuffled=False)
+
+    def test_count_deck(self):
+        self.assertEqual(len(self.shuffled_deck.deck), 52)
+
+
+class TexasHoldemHand(object):
+
+    def __init__(self, card1=None, card2=None):
+        self.card1 = card1
+        self.card2 = card2
+
+    @property
+    def suitedness(self):
+        if self.card1.suit == self.card2.suit:
+            return True
+        else: 
+            return False
+
+
+if __name__ == '__main__':
+    '''
+    '''
+    unittest.main()
+    
+
