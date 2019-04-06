3880fd4d92897fc1ea4839905c42dd9037f6d332
Trent Robbins <robbintt@gmail.com>
2019-04-06 00:20:48 -0700

    Added parts of some texas holdem objects and some evaluation.

--- deck.py
+++ deck.py
@@ -30,6 +30,18 @@ RANK_NAMES = {
     'A' : 'ace'
 }
 
+HAND_RANKS = (
+    'high_card',
+    'pair',
+    '2_pair',
+    '3_kind',
+    'straight',
+    'flush',
+    'full_house',
+    '4_kind',
+    'straight_flush',
+    'royal_flush'
+)
 
 class Card(object):
     '''
@@ -40,7 +52,10 @@ class Card(object):
     
 
 class Deck(object):
-    '''
+    ''' Deck for dealing cards
+
+    This deck only supports dealing from the top and shuffling.
+    For the purposes of this deck, python list.pop() deals from the top.
     '''
 
     def __init__(self, suits, ranks, shuffled=True):
@@ -51,7 +66,13 @@ class Deck(object):
         self.shuffled = shuffled
         self.deck = [Card(s, r) for s in self.suits for r in self.ranks]
         if self.shuffled:
-            random.shuffle(self.deck)
+            self.shuffle()
+
+    def deal(self):
+        return self.deck.pop()
+
+    def shuffle(self):
+        random.shuffle(self.deck)
 
     
 class StandardDeckPropertiesTest(unittest.TestCase):
@@ -77,6 +98,36 @@ class TexasHoldemHand(object):
         self.card2 = card2
 
     @property
+    def suited(self):
+        if self.card1.suit == self.card2.suit:
+            return True
+        else: 
+            return False
+
+    @property
+    def suited_str(self):
+        if self.card1.suit == self.card2.suit:
+            return 's'
+        else: 
+            return 'o'
+
+    def __str__(self):
+        return("{}{}-{}{} : {}{}{}".format(
+            self.card1.rank, self.card1.suit, 
+            self.card2.rank, self.card2.suit,
+            self.card1.rank, self.card2.rank, self.suited_str))
+
+
+class TexasHoldemCommunityCards(object):
+
+    def __init__(self, card1=None, card2=None, card3=None, card4=None, card5=None):
+        self.card1 = card1
+        self.card2 = card2
+        self.card3 = card3
+        self.card4 = card4
+        self.card5 = card5
+
+    @property
     def suitedness(self):
         if self.card1.suit == self.card2.suit:
             return True
@@ -84,11 +135,35 @@ class TexasHoldemHand(object):
             return False
 
 
+
+class HandEvaluator(object):
+    ''' Build and evaluate a hand, and score it
+
+    community_cards = a container of 5 cards
+    hand = a container of 2 cards
+
+    get the class of hand and rank of class
+    e.g. ('flush', '13') - ace high flush
+    write a little scoring matrix from low to high that can be enumerated
+    identify each score that can be made from the five cards and keep the highest
+    or test each from the top and keep the first one you get
+    '''
+    def __init__(self, hand, community_cards):
+        self.hand = hand
+        self.community_cards = community_cards
+        self.all_cards = self.hand + self.community_cards
+
+
 if __name__ == '__main__':
     '''
     '''
-    print("Unshuffled", ["{}{}".format(card.suit, card.rank) for card in Deck(SUITS, RANKS, shuffled=False).deck])
-    print("Shuffled", ["{}{}".format(card.suit, card.rank) for card in Deck(SUITS, RANKS, shuffled=True).deck])
+    shuffled_deck = Deck(SUITS, RANKS)
+    unshuffled_deck = Deck(SUITS, RANKS, shuffled=False)
+    print("Unshuffled", ["{}{}".format(card.suit, card.rank) for card in unshuffled_deck.deck])
+    print("Shuffled", ["{}{}".format(card.suit, card.rank) for card in shuffled_deck.deck])
+    hand = TexasHoldemHand(shuffled_deck.deal(), shuffled_deck.deal())
+    print("Hand: {}".format(hand))
+
     unittest.main()
     
 
